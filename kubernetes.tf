6abdd4086b6979171ca20b664d0ec1d3e3a5c37a
AfonsoCastro1983 <afonso.castro@gmail.com>
2024-09-28 06:59:04 -0400

    Alterações de permissão

--- kubernetes.tf
+++ kubernetes.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "eks_sg" {
 resource "aws_eks_cluster" "eks" {
   name = "lanchoneteFIAP"
   version = "1.27"
-  role_arn = "arn:aws:iam::992382363343:role/eks-admin"
+  role_arn = "arn:aws:iam::992382363343:role/techChallenge-FIAP-EksClusterRole-hA8fsjaSWHrh"
 
   vpc_config {
     subnet_ids         = ["subnet-058d5839f28fa3c6e", "subnet-0cc7b890158d04759"]
@@ -58,7 +58,7 @@ resource "aws_eks_cluster" "eks" {
 # EKS Node Group
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name = aws_eks_cluster.eks.name
-  node_role_arn = "arn:aws:iam::992382363343:role/eks-admin"
+  node_role_arn = "arn:aws:iam::992382363343:role/techChallenge-FIAP-EksNodeGroupRole-7l3lSBqPBXCb"
   subnet_ids = ["subnet-058d5839f28fa3c6e", "subnet-0cc7b890158d04759"]
 
   scaling_config {
@@ -69,4 +69,32 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   instance_types = ["t3.small"]
   ami_type       = "AL2_x86_64"
+}
+
+resource "null_resource" "eks_auth" {
+  provisioner "local-exec" {
+    command = <<EOT
+      kubectl apply -f - <<EOF
+      apiVersion: v1
+      kind: ConfigMap
+      metadata:
+        name: aws-auth
+        namespace: kube-system
+      data:
+        mapRoles: |
+          - rolearn: ${aws_iam_role.eks_admin.arn}
+            username: system:node:{{EC2PrivateDNSName}}
+            groups:
+              - system:bootstrappers
+              - system:nodes
+        mapUsers: |
+          - userarn: arn:aws:iam::992382363343:user/Afonso
+            username: admin
+            groups:
+              - system:masters
+      EOF
+    EOT
+  }
+
+  depends_on = [aws_eks_cluster.eks]
 }
